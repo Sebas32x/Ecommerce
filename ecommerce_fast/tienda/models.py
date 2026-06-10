4cc28a71e25fe800d8215ec38e6a228a1784139b
Sebas32x <sebarexoy@gmail.com>
2026-06-09 22:33:38 -0300

    Agrego usuarios al model para la bd

--- ecommerce_fast/tienda/models.py
+++ ecommerce_fast/tienda/models.py
@@ -17,9 +17,35 @@ class Producto(models.Model):
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField(default=0)
     imagen_url = models.URLField(blank=True) # Para meter un link de imagen rápido
-
+    
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
     def __str__(self):
         return self.nombre
+
+class User(models.Model):
+    nombreDeUsuario = models.CharField(max_length=40)
+    nombreYApellido = models.CharField(max_length=40)
+    CURSO_OPCIONES = [
+        ("Preceptor", "preceptor"),
+        ("Profesor", "profesor"),
+        ("Directivo", "directivo"),
+        
+        ("1-1"),("1-2"),("1-3"),
+        ("2-1"),("2-2"),("2-3"),
+        ("3-1"),("3-2"),("3-3"),
+        ("4-2"),("4-3"),("4-4"),
+        ("5-2"),("5-3"),
+        ("6-2"),("6-3"),
+        ("7-2"),("7-3"),
+        
+        ("X")
+    ]
+    curso = models.CharField(
+        max_length=3,
+        choices=CURSO_OPCIONES,
+        default="X"
+    )
+    contraseña = models.CharField(max_length=64) #Obligatorio sha256
+    
